64f65124bfc15e59c6d7f82e1b75f9bd4542675e
gyoofe <pwww1111@gmail.com>
2019-05-28 09:37:55 +0000

    result update

--- pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
+++ pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
@@ -92,6 +92,8 @@ class FooEnv6(env_base.FooEnvBase):
 
         #reward 수정 예정,, torque 총합 페널티..
         #reward = alive_bonus - np.exp(2*(np.abs(self.leftAngle)) + 1.5*walkPenalty + 2*velocityReward)
+
+        ##초반 walkpenalty 상쇄?
         reward = alive_bonus - 2*self.leftAngle - 1.5*walkPenalty - tausums/1000
 
 
