635b93058e3ed12607158fc2b326e2a3cc2501ea
2018103173 <2018103173@hanyang.ac.kr>
2019-07-08 20:23:22 +0900

    env modify

--- gym-foo/gym_foo3d/envs/foo_env6.py
+++ gym-foo/gym_foo3d/envs/foo_env6.py
@@ -15,127 +15,223 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-from . import env_base
-
 import threading
 import time
 
 import matplotlib
 import matplotlib.pyplot as plt
 
+from . import env_base
+
 skel_path="/home/qfei/dart/data/sdf/atlas/"
-env_num=6
+class FooEnv6(env_base.FooEnvBase):
+    def init_sim(self,cDirection,render):
+        super().init_sim(cDirection,render)
+        observation_spaces = np.concatenate([self.sim.skeletons[1].q[1:3],self.sim.skeletons[1].q[6:9],self.sim.skeletons[1].q[14:20],self.sim.skeletons[1].q[26:32],self.sim.skeletons[1].dq[1:3],self.sim.skeletons[1].dq[6:9],self.sim.skeletons[1].dq[14:20],self.sim.skeletons[1].dq[26:32],[int(self.controller.mCurrentStateMachine.mCurrentState.mName),0,0]])
+        self.action_space = spaces.Box(low = 0, high = 1.5, shape=(16,))
+        observation_spaces = np.zeros(len(observation_spaces))
+        self.observation_space =spaces.Box(observation_spaces, -observation_spaces)
 
-class FooEnv6(env_base.FooEnvBase_rs):
+        #Queue 크기 설정
+        #self.XveloQueue = env_base.CircularQueue(8)
+        #self.ZveloQueue = env_base.CircularQueue(8)
+        
+        self.step_counter = 0
+        self.the_last = 0
+        self.a = [1,0,0]
+        self.reward_counter = 0
+        self.state_reward = 0
+        print(self.targetAngle)
 
-    def init_sim(self):
-        super().init_sim()
-        self.action_space = spaces.Box(low = 0, high = 1.5, shape=(5,))
+    def reset(self):
+        super().reset()
+        self.step_counter = 0
+        self.the_last = 0
+        self.a = [1,0,0]
+        self.reward_counter = 0
+        self.state_reward = 0
 
+        return self.get_state()
+
+    def get_state(self):
+        return np.concatenate([self.sim.skeletons[1].q[1:3],self.sim.skeletons[1].q[6:9],self.sim.skeletons[1].q[14:20],self.sim.skeletons[1].q[26:32],self.sim.skeletons[1].dq[1:3],self.sim.skeletons[1].dq[6:9],self.sim.skeletons[1].dq[14:20],self.sim.skeletons[1].dq[26:32],[int(self.controller.mCurrentStateMachine.mCurrentState.mName),self.desiredSpeed,self.leftAngle]])
+ 
 
     def step(self, action):
-        #print(action)
         pos_before = self.sim.skeletons[1].com()
-        #self.sim.skeletons[1].
         panelty = 0
         check = 0
-        #print(action)
-        #input()
-        action = self.clip_Scaling_Actiond5(action)
+        action = self.clip_Scaling_Actiond10(action)
 
-        self.do_simulation(action,self.frameskip)
-
+        self.previousState = self.controller.mCurrentStateMachine.mCurrentState.mName
+        #done은 에피소드가 끝났는지..
+        done,n_frames = self.do_simulation(action)
         
-        ##다리사이 각 계산 ZY평면
+        #발의 위치
         r_foot_pos = self._getJointPosition(self.r_foot) 
         l_foot_pos = self._getJointPosition(self.l_foot)
+        """
+        pos_after = self.sim.skeletons[1].com()
+        self.XveloQueue.enqueue(pos_after[0])
+        self.ZveloQueue.enqueue(pos_after[2])
+        """
+        #1초간의 속도 계산
+
+        ###수정 예정(env_base에서도 수정해야됨)###
+        #velocity_s = self.distance()
+        #velocityReward = np.abs(velocity_s - self.desiredSpeed)
+        #print("vs",velocity_s)
+        #print("dS", self.desiredSpeed)
 
         """
-        l_uleg_jpos = self._getJointPosition(self.l_uleg)
-        r_uleg_jpos = self._getJointPosition(self.r_uleg)
-        l_lleg_jpos = self._getJointPosition(self.l_lleg)
-        r_lleg_jpos = self._getJointPosition(self.r_lleg)
+        alive_bonus = 10
 
-        rLeg_vector = r_lleg_jpos - self.pelvis.world_transform()[:3,3]
-        lLeg_vector = l_lleg_jpos - self.pelvis.world_transform()[:3,3]
+        #방향 맞춤
+        self.currentFrameXAxis = self.getCOMFrameXAxis()
+        self.leftAngle = self._calAngleBetweenVectors(self.currentFrameXAxis, self.targetFrameXAxis)
+        if np.cross(self.currentFrameXAxis, self.targetFrameXAxis)[1] < 0:
+            self.leftAngle = -self.leftAngle
 
-        pelvisX_axis = self.pelvisX.axis_in_world_frame()
 
-        rLeg_vector = np.cross(pelvisX_axis, np.cross(rLeg_vector,pelvisX_axis))
-        lLeg_vector = np.cross(pelvisX_axis, np.cross(lLeg_vector, pelvisX_axis))
-        #print(rLeg_vector)
-        #print(lLeg_vector)
-        #LegZYangle = self._calAngleBetweenVectors(rLeg_vector,lLeg_vector)
-
-        #anglesPanelty = 0
-        #if LegZYangle > 0.55:
-        #    anglesPanelty = anglesPanelty + LegZYangle * 0.3
-        """
-
-        ###
-        pos_after = self.sim.skeletons[1].com() 
-        velocityReward = np.abs(self.veloQueue.mean() - self.desiredSpeed)*100
-        #print((r_foot_pos + l_foot_pos)/2)
-        #print(pos_after)
-        foot_balance = (r_foot_pos[0] + l_foot_pos[0])/2 - pos_after[0] if (r_foot_pos[0] + l_foot_pos[0])/2 - pos_after[0] > 0 else 0 
-        ##직선 보행 panelty
-        y_lane = np.abs(pos_after[2])
-        alive_bonus = 9
-        reward = alive_bonus - velocityReward*0.9 -y_lane*0.2 - (np.abs(self.skel.q[0] + np.pi*0.5) + np.abs(self.skel.q[1]) + np.abs(self.skel.q[2]))*0.2 - foot_balance
-
-        if pos_after[1] < 0.025 or pos_after[1] > 0.5:
-            done = True
-        elif r_foot_pos[1] > pos_after[1]:
-            done = True
-        elif l_foot_pos[1] > pos_after[1]:
-            done = True
-        elif self.step_counter > self.step_per_sec* 60:
-            done = True
-        elif pos_after[0] < -2:
-            done = True
+        #walkPenalty(직선보행 페널티)
+        ###Queue 수정해야됨!!!!!!!!!!!!! ###
+        if self.XveloQueue.f_e_d() == 0 and self.ZveloQueue.f_e_d() == 0:
+            self.a = [1,0,0]
         else:
-            done = False
-        self.step_counter += self.frameskip
-        self.speedChangeCounter += self.frameskip
+            self.a = [self.XveloQueue.f_e_d(), 0, self.ZveloQueue.f_e_d()]
+        self.a = cMat.Matrix.normalize(self.a)
+        walkPenalty = self._calAngleBetweenVectors(self.currentFrameXAxis, self.a)
+        """
+        #print(self.skel.tau)
+        #tausums = 0
+        #for i in self.skel.tau:
+        #    tausums += np.abs(i)
+
+        #print(tausums)
+        #input()
+ 
+
+        #reward 수정 예정,, torque 총합 페널티..
+        #reward = alive_bonus - np.exp(2*(np.abs(self.leftAngle)) + 1.5*walkPenalty + 2*velocityReward)
+
+        ##초반 walkpenalty 상쇄?
+        #reward = alive_bonus - walkPenalty - np.abs(self.leftAngle) - self.tausums/1000
+        #reward = alive_bonus - walkPenalty - np.abs(self.leftAngle)
+
+        reward = self.state_reward/(self.reward_counter)
+
+        self.step_counter += n_frames
         thisState = self.get_state()
-        thispos = pos_after[0]
+       
         self.actionSteps += 1
         self.episodeTotalReward += reward
+        self.set_desiredSpeed()
 
+
+        #수정
+        #if self.actionSteps % (self.step_per_walk * 20) == self.step_per_walk*5 and self.cDirection and self.step_counter is not 0:
+            #print(self.step_counter)
+            #input()
+            #self.changeDirection()
+            ###MAXtime수정할것!!!!!!!!!!!!!!!!!!1
+            #self.change_targetspeed()
+        #if self.step_counter == self.step_per_sec * 30 and self.cDirection:
+        #    self.changeDirection()
+
+        
         if done is True:
             print("episodeDone... mean Reward: " + str(self.episodeTotalReward/self.actionSteps))
-            print("velocityReward: " + str(velocityReward) + "__" + str(self.veloQueue.mean()))
-            self.reset()
+            #print("velocityReward: " + str(velocityReward) + "__" + str(velocity_s)+ "__" + str(self.desiredSpeed))
+            print("action Step", self.actionSteps,self.step_counter)
+            #self.reset()
+         
+        info = {
+                'env_step':self.step_counter,
+                'n_frames':n_frames
+        }
 
-        self.changeSpeed(self.step_per_sec,5)
-        return thisState, reward, done, pos_after
+        #print(reward)
+        #print(done)
+        #print(self.previousState)
+        #print(self.step_counter)
+        #print(n_frames)
+        #input()
+
+        if done is True:
+            return thisState, 0, done, info
+        return thisState, reward, done, info 
 
         #self.do_simulation(action, 60)
 
 
 
-    def do_simulation(self, action, n_frames):
-        """set action..
-            for _ in range(n_frames):
-                self.simulator step
-        """
-        #print(action)
-        #input()
-        #self.controller.update(action)
-        for _ in range(n_frames):
-            #self.controller.update(None)
-            self.controller.update(action,2)
+    def do_simulation(self, action):
+        self.controller.mCurrentStateMachine.mCurrentAction = action
+        self.controller.mCurrentStateMachine.setTrainedDesiredAction(action, 0)
+        done = False
+        n_frames = 0
+        self.reward_counter = 0
+        self.state_reward = 0
+
+        self.tausums = 0
+        while(self.previousState is self.controller.mCurrentStateMachine.mCurrentState.mName):
+            self.controller.update()
             self.sim.step()
+            
+            if self.tausums is 0:
+                for i in self.skel.tau:
+                    self.tausums += np.abs(i)
+
+            pos_after = self.sim.skeletons[1].com()
+            r_foot_pos = self._getJointPosition(self.r_foot) 
+            l_foot_pos = self._getJointPosition(self.l_foot)
+            
+            if (n_frames+self.the_last)%30 is 0 or self.previousState is not self.controller.mCurrentStateMachine.mCurrentState.mName:
+                #print(n_frames)
+                self.XveloQueue.enqueue(pos_after[0])
+                self.ZveloQueue.enqueue(pos_after[2])
+                alive_bonus = 10
+
+                #방향 맞춤
+                self.currentFrameXAxis = self.getCOMFrameXAxis()
+                self.leftAngle = self._calAngleBetweenVectors(self.currentFrameXAxis, self.targetFrameXAxis)
+                if np.cross(self.currentFrameXAxis, self.targetFrameXAxis)[1] < 0:
+                    self.leftAngle = -self.leftAngle
+
+
+                #walkPenalty(직선보행 페널티)
+                ###Queue 수정해야됨!!!!!!!!!!!!! ###
+                if self.XveloQueue.f_e_d() == 0 and self.ZveloQueue.f_e_d() == 0:
+                    self.a = [1,0,0]
+                else:
+                    self.a = [self.XveloQueue.f_e_d(), 0, self.ZveloQueue.f_e_d()]
+                self.a = cMat.Matrix.normalize(self.a)
+                walkPenalty = self._calAngleBetweenVectors(self.currentFrameXAxis, self.a)
+                reward = alive_bonus - walkPenalty - np.abs(self.leftAngle)
+                
+                self.state_reward += reward
+                self.reward_counter += 1
+
             if(self.isrender):
                 time.sleep(0.001)
-            speed = self.sim.skeletons[1].com_velocity()
-            self.veloQueue.enqueue(speed[0])
+            if pos_after[1] < 0.020 or pos_after[1] > 0.5:
+                done = True
+            #정면으로 걷지않을경우 빠르게 종료
+            #elif np.abs(pos_after[2]) > 2:
+            #    done = True
+            elif r_foot_pos[1] > pos_after[1]:
+                done = True
+            elif l_foot_pos[1] > pos_after[1]:
+                done = True
+            elif self.actionSteps > self.step_per_walk * 100:
+                done = True
+            if done is True:
+                break
+            n_frames += 1
 
-        #time.sleep(1)
-        #input()
+        self.the_last = n_frames%30
+        return done,n_frames
+   
+    def render(self):
         return
-        #speed = self.sim.skeletons[1].com_velocity()
-        #self.veloQueue.enqueue(speed[0])
- 
-
 
