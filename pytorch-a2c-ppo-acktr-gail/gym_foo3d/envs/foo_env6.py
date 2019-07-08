4c9a0fc8d3180e613cca6fb96fc1080b172a4141
gyoofe <pwww1111@gmail.com>
2019-07-08 09:14:41 +0000

    result update

--- pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
+++ pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
@@ -132,7 +132,7 @@ class FooEnv6(env_base.FooEnvBase):
         #reward = alive_bonus - np.exp(2*(np.abs(self.leftAngle)) + 1.5*walkPenalty + 2*velocityReward)
 
         ##초반 walkpenalty 상쇄?
-        reward = alive_bonus - self.tausums/2000 - walkPenalty - np.abs(self.leftAngle)
+        reward = alive_bonus - self.tausums/8000 - walkPenalty - np.abs(self.leftAngle)
 
 
         self.step_counter += n_frames
