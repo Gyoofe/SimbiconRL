53e77d76a479f0e1cebf6b66642e778c32a3593a
gyoofe <pwww1111@gmail.com>
2019-09-11 07:40:49 +0000

    result update

--- pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
+++ pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
@@ -181,7 +181,7 @@ class FooEnv6(env_base.FooEnvBase):
         action[8] = ((action[8]-1)/2)*math.radians(30.0)
         action[9] = (action[9])*math.radians(30.0) 
         ##contact offset
-        #action[10] = action[10]*150
+        action[10] = action[10]*150
 
         ##root
         #action[14] = (action[14])*np.pi/4
@@ -322,8 +322,9 @@ class FooEnv6(env_base.FooEnvBase):
         ##다리 질질끌고 통통 튀면서 걷고 한쪽 다리 거의 못들어올리고 방향전환은 가능하나 결과 별로 좋지않다.
         ##reward = alive_bonus - self.tausums/8000 - 2*walkPenalty - 2*np.abs(self.leftAngle) - 1.4*np.abs(DisV - 1) - 3*torsoMSE - 4*FootstepDiff
         
-        reward = (alive_bonus - self.tausums/8000 - 5*walkPenalty - 5*np.abs(self.leftAngle) - 1.4*np.abs(DisV - 0.7) - 3*torsoMSE - 2*FootstepDiff)*(n_frames/SIMULATION_STEP_PER_SEC)
+        #reward = (alive_bonus - self.tausums/8000 - 5*walkPenalty - 5*np.abs(self.leftAngle) - 1.4*np.abs(DisV - 0.7) - 3*torsoMSE - 2*FootstepDiff)*(n_frames/SIMULATION_STEP_PER_SEC)
         #reward = (alive_bonus - self.tausums/8000 - 5*walkPenalty - 5*np.abs(self.leftAngle) - 1.4*np.abs(DisV - 1) - 3*torsoMSE - 2*FootstepDiff)
+        reward = (alive_bonus - self.tausums/8000 - 5*walkPenalty - 5*np.abs(self.leftAngle) - 4*np.abs(DisV - 1) - 3*torsoMSE - 2*FootstepDiff)
 
 
         self.step_counter += n_frames
@@ -337,9 +338,9 @@ class FooEnv6(env_base.FooEnvBase):
         
         #수정
         #if self.actionSteps % (self.step_per_walk * 20) == self.step_per_walk*5 and self.cDirection and self.step_counter is not 0 and self.curValue > 0:
-        #if self.actionSteps % (self.step_per_walk * 10) == self.step_per_walk*5 and self.cDirection and self.step_counter is not 0:
-        if self.change_step % (SIMULATION_STEP_PER_SEC*10) > SIMULATION_STEP_PER_SEC*5 and self.change_step > 0 and self.cDirection:
-            self.change_step = self.change_step - SIMULATION_STEP_PER_SEC*10
+        if self.actionSteps % (self.step_per_walk * 10) == self.step_per_walk*5 and self.cDirection and self.step_counter is not 0:
+        #if self.change_step % (SIMULATION_STEP_PER_SEC*10) > SIMULATION_STEP_PER_SEC*5 and self.change_step > 0 and self.cDirection:
+            #self.change_step = self.change_step - SIMULATION_STEP_PER_SEC*10
 
             #print(self.step_counter)
             #input()
@@ -528,8 +529,8 @@ class FooEnv6(env_base.FooEnvBase):
                 done = True
             elif l_foot_pos[1] > pos_after[1]:
                 done = True
-            #elif self.actionSteps > self.step_per_walk * 200:
-            elif self.step_counter > SIMULATION_STEP_PER_SEC*40:
+            elif self.actionSteps > self.step_per_walk * 200:
+            #elif self.step_counter > SIMULATION_STEP_PER_SEC*40:
                 done = True
             if done is True:
                 break
