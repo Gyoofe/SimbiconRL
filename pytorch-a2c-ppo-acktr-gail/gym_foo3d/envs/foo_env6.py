1a0dcbefb9fba4191130b64ce09cbd9ffe0c7167
gyoofe <pwww1111@gmail.com>
2020-03-29 21:21:59 +0900

    b_result update

--- pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
+++ pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
@@ -503,7 +503,7 @@ class FooEnv6(env_base.FooEnvBase):
 
         alive_bonus = ALIVE_BONUS
 
-        reward = (alive_bonus - 2*rootPenalty - np.abs(pos_after[2]) - 12*StepLengthPenalty - 10*FootHeightPenalty - 15*stepDurationPenalty - 8*torsoUprightPenalty)/(3/self.desiredStepDuration)
+        reward = (alive_bonus - 2*rootPenalty - np.abs(pos_after[2]) - 12*StepLengthPenalty - 10*FootHeightPenalty - 15*stepDurationPenalty - 8*torsoUprightPenalty)
 
 
         self.step_counter += n_frames
