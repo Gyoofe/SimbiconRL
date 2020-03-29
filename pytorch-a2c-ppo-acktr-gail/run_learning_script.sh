d93ed5382908dca847be7ce1f7e9824cd8a61797
gyoofe <pwww1111@gmail.com>
2020-03-29 21:38:09 +0900

    g result update

---
+++ pytorch-a2c-ppo-acktr-gail/run_learning_script.sh
@@ -0,0 +1,1 @@
+python3 main.py --algo ppo --lr 1e-4 --gamma 0.99 --use-gae --gae-lambda 0.95 --entropy-coef 0.01 --value-loss-coef 0.5 --num-processes 32 --num-steps 512 --ppo-epoch 10 --num-mini-batch 2048 --clip-param 0.2 --log-interval 1 --save-interval 10 --num-env-steps 10000000000 --env-name 3d-v6 --use-proper-time-limits
