8e980bf8fa0dba989224ee8d237ce74774c436eb
prangle <thscowns@gmail.com>
2020-07-01 15:03:06 +0000

    result update

--- pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
+++ pytorch-a2c-ppo-acktr-gail/gym_foo3d/envs/foo_env6.py
@@ -310,6 +310,12 @@ class FooEnv6(env_base.FooEnvBase):
         action[23] = (action[23])*math.radians(5.0)
         #swax13
         action[24] = action[24]*math.radians(5.0)
+
+        #swax02 
+        action[25] = (action[25])*math.radians(5.0)
+        #swax13
+        action[26] = action[26]*math.radians(5.0)
+
         return action
 
 
@@ -504,13 +510,6 @@ class FooEnv6(env_base.FooEnvBase):
                 np.exp(-9*np.square(FootHeightPenalty)))
         """
 
-<<<<<<< HEAD
-        #reward = (alive_bonus - self.tausums/8000 - 5*walkPenalty - 5*np.abs(self.currentLeftAngle) - 3*rootPenalty - 8*StepLengthPenalty - 8*FootHeightPenalty - 8*stepDurationPenalty - 10*torsoUprightPenalty)
-
-        alive_bonus = ALIVE_BONUS
-
-        reward = (alive_bonus -2*rootPenalty - np.abs(pos_after[2]) - 12*StepLengthPenalty - 10*FootHeightPenalty - 15*stepDurationPenalty - 8*torsoUprightPenalty)/(3/self.desiredStepDuration)
-=======
         alive_bonus = ALIVE_BONUS
 
         #reward = (alive_bonus - self.tausums/32000 - 5*walkPenalty - 5*np.abs(self.currentLeftAngle) - 3*rootPenalty - 8*StepLengthPenalty - 8*FootHeightPenalty - 8*stepDurationPenalty - 10*torsoUprightPenalty)
@@ -518,8 +517,6 @@ class FooEnv6(env_base.FooEnvBase):
 
 
         reward = (alive_bonus - self.tausums/32000 - np.abs(pos_after[2]) - 12*StepLengthPenalty - 10*FootHeightPenalty - 15*stepDurationPenalty - 8*torsoUprightPenalty)/(3/self.desiredStepDuration)
-
->>>>>>> 38951f38... reward bugfix
 
         self.step_counter += n_frames
         self.change_step += n_frames
